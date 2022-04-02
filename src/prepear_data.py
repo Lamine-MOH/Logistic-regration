970c032bbe0dd87cdc02a03c5baa52b2204bbfc5
MOH Lamine <mmmbbbhhh17@gmail.com>
2022-04-02 12:56:33 +0100

    update the data

--- src/prepear_data.py
+++ src/prepear_data.py
@@ -2,68 +2,138 @@ import csv
 from random import random
 import sys
 
-location = sys.argv[1]
-learn_file_size = int(sys.argv[2]) if len(sys.argv) >= 3 and int(sys.argv[2]) >= 0 and int(sys.argv[2]) <= 100 else 70
+data_location = sys.argv[1]
+training_file_size = int(sys.argv[2]) if len(sys.argv) >= 3 and int(sys.argv[2]) >= 0 and int(sys.argv[2]) <= 100 else 75
 
 # calc the data length #
-with open(location, "r") as csv_file:
-    csv_reader = csv.reader(csv_file)
-    next(csv_reader)
-    
-    data_length = 0
+yes_counter = 0
+no_counter = 0
+with open(data_location, "r") as csv_file: # open the data file for reading
+    csv_reader = csv.reader(csv_file) # create csv reader
+    next(csv_reader) # skip the features names
+
     for line in csv_reader:
-        data_length += 1
+        if line[0] == "Yes":
+            yes_counter += 1
+        else:
+            no_counter += 1
+            
+data_length = yes_counter*2 if yes_counter < no_counter else no_counter*2
 #
 
-with open(location, "r") as csv_file:
-    csv_reader = csv.reader(csv_file)
-    next(csv_reader)
+csv_file_read_for_yes_values = open(data_location, "r", newline="") # create a file reader for the yes values
+csv_file_read_for_no_values = open(data_location, "r", newline="")  # create a file reader for the no values
 
-    with open("data/learn data.csv", "w", newline="") as csv_learn_file:
-        with open("data/test data.csv", "w", newline="") as csv_test_file:
-            csv_learn_writer = csv.writer(csv_learn_file)
-            csv_test_writer = csv.writer(csv_test_file)
+csv_yes_reader = csv.reader(csv_file_read_for_yes_values) # create a csv reader for the yes values
+csv_no_reader = csv.reader(csv_file_read_for_no_values)   # create a csv reader for the yes values
+next(csv_yes_reader) # skip the featers names
+next(csv_no_reader)  # skip the featers names
+#
+
+csv_training_file = open("data/training_data.csv", "w", newline="") # create a file writer for training data
+csv_test_file  = open("data/test_data.csv", "w", newline="")        # create a file writer for test data
+
+csv_training_writer = csv.writer(csv_training_file) # create a csv writer for trainign data
+csv_test_writer = csv.writer(csv_test_file)         # create a csv writer for test data
+#
+
+def convert_line(line):
+    new_line = []
+    
+    for feature in line[1:]:
+        # for the Smoking, AlcoholDrinking, Stroke, DiffWalking, Diabetic, PhysicalActivity, Asthma, KidneyDisease, SkinCancer columns
+        if feature.__contains__("Yes"):
+            new_line.append(1)
+        elif feature.__contains__("No"):
+            new_line.append(0)
+        
+        # for the six column
+        elif feature == "Male":
+            new_line.append(1)
+        elif feature == "Female":
+            new_line.append(2)
+        #
             
-            learn_file_counter = 0
-            test_file_counter = 0
+        # for the AgeCategory column
+        elif feature.__contains__("-"):
+            new_line.append( sum([ int(value) for value in line[9].split("-") ]) / 2 ) # the Average
+        elif feature.__contains__(" or older"):
+            new_line.append( feature.replace(" or older", "") )
+        #
+        
+        # for the Race column
+        elif feature.__contains__("White"):
+            new_line.append(1)
+        elif feature.__contains__("Black"):
+            new_line.append(2)
+        elif feature.__contains__("American Indian/Alaskan Native"):
+            new_line.append(3)
+        elif feature.__contains__("Asian"):
+            new_line.append(4)
+        elif feature.__contains__("Hispanic"):
+            new_line.append(5)
+        elif feature.__contains__("Other"):
+            new_line.append(6)
+        #
+        
+        # for the GenHealth column
+        elif feature.__contains__("Poor"):
+            new_line.append(1)
+        elif feature.__contains__("Fair"):
+            new_line.append(2)
+        elif feature.__contains__("Good"):
+            new_line.append(3)
+        elif feature.__contains__("Very good"):
+            new_line.append(4)
+        elif feature.__contains__("Excellent"):
+            new_line.append(5)
+            
+        # for the BMI, PhysicalHealth, MentalHealth, SleepTime columns
+        else:
+            new_line.append(feature)
+    
+    # add the output at the end (HeartDisease)
+    if line[0].__contains__("Yes"):
+        new_line.append(1)
+    elif line[0].__contains__("NO"):
+        new_line.append(0)
+    #
+    
+    return new_line
 
-            for line in csv_reader:
-                
-                new_line = []
-                new_line.append(line[1])
-                new_line.append(1 if line[2] == "Yes" else 0)
-                new_line.append(1 if line[3] == "Yes" else 0)
-                new_line.append(1 if line[4] == "Yes" else 0)
-                new_line.append(line[5])
-                new_line.append(line[6])
-                new_line.append(1 if line[7] == "Yes" else 0)
-                new_line.append(1 if line[8] == "Male" else 2)
-                new_line.append(80 if line[9].__contains__("or older") else sum([ int(value) for value in line[9].split("-") ]) / 2)
-                new_line.append(1 if line[11].__contains__("Yes") else 0)
-                new_line.append(1 if line[12] == "Yes" else 0)
-                new_line.append(line[14])
-                new_line.append(1 if line[15] == "Yes" else 0)
-                new_line.append(1 if line[16] == "Yes" else 0)
-                new_line.append(1 if line[17] == "Yes" else 0)
-                new_line.append(1 if line[0] == "Yes" else 0)
-                
-                
-                
-                if learn_file_counter >= data_length * learn_file_size / 100:
-                    csv_test_writer.writerow(new_line)
-                elif test_file_counter >= data_length * (100 - learn_file_size) / 100:
-                    csv_learn_writer.writerow(new_line)
-                else:
-                    if random() <= learn_file_size / 100:
-                        csv_learn_writer.writerow(new_line)
-                        learn_file_counter += 1
-                    else:
-                        csv_test_writer.writerow(new_line)
-                        test_file_counter += 1
-                        
-                        
-                    
-                    
-                
-                
+def write(writer):
+    for line in csv_yes_reader:
+        if line[0] == "Yes":
+            writer.writerow( convert_line(line) ) # write line with yes output
+            break
+            
+    for line in csv_no_reader:
+        if line[0] == "No":
+            writer.writerow( convert_line(line) )# write line with no output
+            break
+# 
 
+training_counter = int(data_length * (training_file_size/100))
+test_counter = data_length - training_counter
+
+for i in range(int(data_length/2)):            
+    
+    # split the data randomly
+    if training_counter <= 0:
+        write(csv_test_writer)
+        test_counter -= 2
+    elif test_counter <= 0:
+        write(csv_training_writer)
+        training_counter -= 2
+    else:
+        if random() <= training_file_size / 100:
+            write(csv_training_writer)
+            training_counter -= 2
+        else:
+            write(csv_test_writer)
+            test_counter -= 2
+            
+csv_file_read_for_yes_values.close()
+csv_file_read_for_no_values.close()
+csv_training_file.close()
+csv_test_file.close()
